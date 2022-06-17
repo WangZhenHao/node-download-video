0df400267613f342eaeae2b807dd304f335a92ed
wangzhenhao <1471047476@qq.com>
2022-06-17 22:53:36 +0800

    【fix】推特视频下载案例

--- twitter/index.js
+++ twitter/index.js
@@ -46,9 +46,10 @@ var down = {
       const list = await this.paraseM3u8File(url);
       await mkdirp(dir);
 
+      await this.downFile(HOST + list.shift(), path.join(dir, './1_init.mp4'));
       await this.downloadFiles(dir, list);
-    //   await this.concatFiles(dir);
-    //   await this.removeFileTs(dir);
+      await this.concatFiles(dir, list);
+      await this.removeFileTs(dir);
       
       return Promise.resolve()
     } catch (e) {
@@ -61,7 +62,8 @@ var down = {
       const files = fs.readdirSync(dir);
       files.forEach((item) => {
         // var re = /\.ts$/;
-        var re = new RegExp(`${POSTFIX}$`)
+        var re = new RegExp('^\\d+.*')
+        // var re2 = new RegExp(`1_init.mp4`)
         if (re.test(item)) {
           fs.unlinkSync(path.join(dir, `./${item}`));
         }
@@ -88,36 +90,48 @@ var down = {
       );
     });
   },
-  concatFiles(dir) {
-    return new Promise((resolve, reject) => {
-      child_process.exec(
-        // `cd ${dir} && ffmpeg -i input.txt -acodec copy -vcodec copy -absf aac_adtstoasc index.mp4`,
-        `cd ${dir} && ffmpeg -f concat -safe 0 -i input.txt -c copy output.mp4`,
-        function (error, stdout, stderr) {
-          if (error) {
-            console.error('合成失败---', error);
-            reject('concatFiles');
-          } else {
-            console.log('合成成功--', stdout);
-            //删除临时文件
-            resolve();
-          }
-        }
-      );
-    });
+  async concatFiles(dir, list) {
+    // return new Promise(async (resolve, reject) => {
+      try {
+        for(let i = 0; i < list.length; i++) {
+          await this.cmd(dir, `ffmpeg -i "concat:1_init.mp4|${i}.m4s" -acodec copy ${i}.mp4`)
+       }
+ 
+       await this.cmd(dir, `ffmpeg -i mp4.txt -acodec copy -vcodec copy -absf aac_adtstoasc output.mp4`)
+
+       return Promise.resolve()
+      } catch(e) {
+        return Promise.reject()
+      }
+      
+      // child_process.exec(
+      //   // `cd ${dir} && ffmpeg -i input.txt -acodec copy -vcodec copy -absf aac_adtstoasc index.mp4`,
+      //   `cd ${dir} && ffmpeg -f concat -safe 0 -i input.txt -c copy output.mp4`,
+      //   function (error, stdout, stderr) {
+      //     if (error) {
+      //       console.error('合成失败---', error);
+      //       reject('concatFiles');
+      //     } else {
+      //       console.log('合成成功--', stdout);
+      //       //删除临时文件
+      //       resolve();
+      //     }
+      //   }
+      // );
+    // });
   },
   downloadFiles(dir, list) {
     return new Promise((reslove, reject) => {
       const fileList = list.map((item, index) => {
         // const name = item.split('/')
-        return `file ${index}${POSTFIX}`;
+        return `file ${index}.mp4`;
       });
       const inputText = ['ffconcat version 1.0', ...fileList];
       let index = 0;
       const self = this;
 
       fs.writeFileSync(
-        path.join(dir, './input.txt'),
+        path.join(dir, './mp4.txt'),
         inputText.join('\n'),
         undefined,
         'utf-8'
@@ -127,7 +141,7 @@ var down = {
         if (list[index]) {
           const url = HOST + list[index];
         //   const name = list[index].split('/')
-          const fileName = path.join(dir, `${index}${POSTFIX}`);
+          const fileName = path.join(dir, `./${index}.m4s`);
 
           self
             .downFile(url, fileName)
@@ -139,7 +153,7 @@ var down = {
               reject('downloadFiles');
             });
         } else {
-          reslove();
+          reslove(fileList);
         }
       }
 
@@ -201,7 +215,7 @@ const POSTFIX = '.m4s'
 // const HOST = 'https://video-hls.yzcdn.cn/';
 // const POSTFIX = '.ts'
 
-// down.download('https://video.twimg.com/amplify_video/1537103411488989184/pl/978x716/3uTkfM9_vE4pkBvv.m3u8?container=fmp4', resovePath('./video/test'))
+down.download('https://video.twimg.com/ext_tw_video/1537333084906995712/pu/pl/640x360/xYpBeMYBdzx9vsra.m3u8?container=fmp4', resovePath('./video/test1'))
 
 // m3u8地址：https://video.twimg.com/amplify_video/1537103411488989184/pl/978x716/3uTkfM9_vE4pkBvv.m3u8?container=fmp4
 // 推特地址：https://twitter.com/TPostMillennial/status/1537103786086391809
@@ -212,13 +226,13 @@ const POSTFIX = '.m4s'
 // getUrlList(text)
 // down.cmd(resovePath('./video/test'), 'ffmpeg -i "concat:0.m4s|2.m4s" -acodec copy 2.mp4')
 
-async function concatFile() {
-  var arr = ['1.m4s', '2.m4s', '3.m4s', '4.m4s', '5.m4s', '6.m4s'];
-  for(let i = 0; i < arr.length; i++) {
-    await down.cmd(resovePath('./video/test'), `ffmpeg -i "concat:0.m4s|${arr[i]}" -acodec copy ${i}.mp4`)
-  }
+// async function concatFile() {
+//   var arr = ['1.m4s', '2.m4s', '3.m4s', '4.m4s', '5.m4s', '6.m4s'];
+//   for(let i = 0; i < arr.length; i++) {
+//     await down.cmd(resovePath('./video/test'), `ffmpeg -i "concat:0.m4s|${arr[i]}" -acodec copy ${i}.mp4`)
+//   }
 
-  down.cmd(resovePath('./video/test'), `ffmpeg -i mp4.txt -acodec copy -vcodec copy -absf aac_adtstoasc output.mp4`)
-}
+//   down.cmd(resovePath('./video/test'), `ffmpeg -i mp4.txt -acodec copy -vcodec copy -absf aac_adtstoasc output.mp4`)
+// }
 
 // concatFile()
